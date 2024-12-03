75dbdc7b64ce0e4855f2009b493b2413b64ac880
nuruddin <sg7720@gmail.com>
2024-12-03 16:19:07 +0300

    оптимизировал код вынес в отдельные компоненты

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@ import styles from "./main.module.scss";
 import React, {useState} from "react";
 import InputField from "@/components/ui/InputField";
 import CustomButton from "@/components/ui/button";
-import {RemoveCircle} from "@mui/icons-material";
-import {IconButton} from "@mui/material";
 import ReactECharts from 'echarts-for-react';
+import {getActualColors, GithubChart} from "@/components/GithubChart";
+import Participant from "@/components/Participant";
+
+
+type SharesData = Array<{ name: string, sum: number, share: string }>
 
 export default function Home() {
     const [name, setName] = useState('');
     const [sum, setSum] = useState('');
-    const [data, setData] = useState<Array<{ name: string, sum: number, share: string }>>([]);
+    const [data, setData] = useState<SharesData>([]);
 
     const handleNameChange = (e: any) => setName(e.target.value);
 
@@ -20,26 +23,31 @@ export default function Home() {
     };
 
     const handleDelete = (id: string) => {
-        let fileterdData = data.filter(el => el.name !== id)
-        setData(fileterdData)
+        let filteredData = data.filter(el => el.name !== id)
+        calculate(filteredData)
     }
 
-    const calculate = () => {
-
-        let current = parseInt(sum)
-        const updatedData = [...data, {name, sum: current, share: 0}];
-
-        const newTotalShare = updatedData.reduce((accumulator, currentValue) => {
+    const calculate = (data: SharesData) => {
+        const newTotalShare = data.reduce((accumulator, currentValue) => {
             return accumulator + currentValue.sum;
         }, 0);
 
-        const finalData = updatedData.map(item => ({
+        const finalData = data.map(item => ({
             ...item,
             share: ((item.sum / newTotalShare) * 100).toFixed(2),
         }));
 
         setData(finalData);
+    }
 
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        if (data.find(el => el.name === name)) {
+            return
+        }
+        let current = parseInt(sum)
+        const updatedData = [...data, {name, sum: current, share: "0"}];
+        calculate(updatedData)
         setName("");
         setSum("");
     }
@@ -48,27 +56,38 @@ export default function Home() {
     return (
         <div className={styles.mainsec}>
             <div className={styles.textBlock}>
-                <h1>Калькулятор долей от общей суммы</h1>
-                <p>У одного 100 тыс руб, у другого 300 тыс. руб - нужно узнать у кого какие доли в процентах</p>
+                <h1>Узнай долю в % от общей суммы</h1>
+                <p>Например: у вас 100 тыс руб, у друга 300 тыс. руб - общая сумма 400 тыс. нужно узнать у кого какие
+                    доли в процентах</p>
             </div>
             <div className={styles.contentBlock}>
                 <div className={styles.dataSide}>
-                    <div className={styles.inpts}>
-                        <InputField placeholder="Имя" onChange={handleNameChange} value={name}/>
-                        <InputField placeholder="Сумма" onChange={handleSumChange} value={sum}/>
-                        <CustomButton onClick={calculate}>=</CustomButton>
+                    <form className={styles.inpts} onSubmit={handleSubmit}>
+                        <InputField
+                            placeholder="Введи имя"
+                            required
+                            onChange={handleNameChange}
+                            value={name}
+                            type={"text"}/>
+                        <InputField
+                            placeholder="Введи свою сумму"
+                            required
+                            onChange={handleSumChange}
+                            value={sum}
+                            className={styles.sumInp}
+                            type={"number"}/>
+                        <div className={styles.calcButton}>
+                            <CustomButton type={"submit"}>=</CustomButton>
+                        </div>
+                    </form>
+                    <div className={styles.horizontalChart}>
+                        <GithubChart data={data}/>
                     </div>
                     <div className={styles.dataWrapper}>
-                        {data.map(el =>
-                            <div key={el.name} className={styles.dataItem}>
-                                <div className={styles.leftData}>
-                                    <span>{el.name}</span>
-                                    <span>{el.sum}</span>
-                                </div>
-                                <span className={styles.sharePercent}>{el.share}%</span>
-                                <IconButton onClick={() => handleDelete(el.name)}><RemoveCircle
-                                    color="error"/></IconButton>
-                            </div>)}
+                        {data.sort((a, b) => b.sum - a.sum).map((el,index) => {
+                            const actualColors = getActualColors(data)
+                            return <Participant person={el} handleDelete={handleDelete} actualColor={actualColors[index]} />
+                        })}
                     </div>
                 </div>
                 <div className={styles.chartSide}>
