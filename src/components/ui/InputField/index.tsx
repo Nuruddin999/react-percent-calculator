0c28525b5256d12d72f2008457117ff3947bc9c1
nuruddin <sg7720@gmail.com>
2024-12-03 16:19:59 +0300

    оптимизировал код вынес в отдельные компоненты

--- src/components/ui/InputField/index.tsx
+++ src/components/ui/InputField/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {HTMLInputProps, InputFieldProps} from "@/components/ui/InputField/types";
+import {InputFieldProps} from "@/components/ui/InputField/types";
 import styles from "./inputfield.module.scss";
 const InputField = (props: InputFieldProps) => {
   return <input  {...props} className={styles.inputField+" "+props.className} />
